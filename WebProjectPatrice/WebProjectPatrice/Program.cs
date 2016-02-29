4cac468d137689da71e73c468f38559f1dcb163f
MelBeee <mel.beee@deepteal.com>
2016-02-28 20:58:52 -0500

    Remodelage du code

--- WebProjectPatrice/WebProjectPatrice/Program.cs
+++ WebProjectPatrice/WebProjectPatrice/Program.cs
@@ -8,25 +8,122 @@ using System.Threading.Tasks;
 
 namespace WebProjectPatrice
 {
-    class Program
+    class CSFile
     {
-        static List<string> keywordsList = new List<string>();
-        static bool    stats    = false,        // option qui crée un fichier de statistiques
-                       keywords = false;        // option qui met les keywords du langage en relief
-        static List<string> csFilesToConvert  = new List<string>();   // tableau contenant les fichiers CS a convertir
-        static Dictionary<string, int> statistiques = new Dictionary<string, int>();  // contient les stats
-        static int NumberOfNumber = 0;
+        private Dictionary<string, int> statistiques = new Dictionary<string, int>();
+        private int NumberOfNumber = 0;
+        private string FileName; 
 
-        static void Main(string[] args)
+        public CSFile(string name)
         {
-            RemplirTableauKeyWord();
-            AnalyseArguments(args);
-            ReadEachLine();
-            if (stats)
-                Statistiques();
+            FileName = name;
         }
 
-        private static void Statistiques()
+        public int getNumberofNumber()
+        {
+            return NumberOfNumber;
+        }
+
+        public Dictionary<string, int> getStatsDictionary()
+        {
+            return statistiques;
+        }
+
+        public void CreateHTML(bool stats, bool keywords, List<string> keywordsList)
+        {
+            Dictionary<string, int> statistiques = new Dictionary<string, int>();
+            string Input;
+            string TemplateStart = "<!DOCTYPE html>\n" +
+                                   "<html>\n" +
+                                   "<head>\n" +
+                                   "<meta charset = 'utf-8' />\n" +
+                                   "<style> span {color:blue;} </style>" +
+                                   "</head><body><pre>\n";
+            string TemplateEnd = "</pre></body>\n" +
+                                 "</html >";
+            StreamWriter fileEnd = new StreamWriter(FileName + ".html");
+            fileEnd.WriteLine(TemplateStart);
+            StreamReader file = new StreamReader(FileName);
+            while ((Input = file.ReadLine()) != null)
+            {
+                string v = Input;
+                if (stats)
+                    FillStatsDictionnary(v);
+                v = Input.Replace("&", "&amp;");
+                v = v.Replace(">", "&gt;");
+                v = v.Replace("<", "&lt;");
+                if (keywords)
+                    v = AddColor(v, keywordsList);
+                fileEnd.WriteLine(v);
+            }
+            fileEnd.WriteLine(TemplateEnd);
+            file.Close();
+            fileEnd.Close();
+        }
+
+        private void FillStatsDictionnary(string line)
+        {
+            string pattern = @"[^\w_]|(\d+\.\d+)";
+            string[] words = Regex.Split(line, pattern);
+            for (int i = 0; i < words.Length; ++i)
+            {
+                if (words[i] != "")
+                {
+                    if ((Regex.IsMatch(words[i], @"^\d+\.\d+")) || (Regex.IsMatch(words[i], @"^\d+")))
+                        NumberOfNumber++;
+                    if (!statistiques.ContainsKey(words[i]))
+                        statistiques.Add(words[i], 1);
+                    else
+                        statistiques[words[i]]++;
+                }
+            }
+        }
+
+        private string AddColor(string line, List<string> keywordsList)
+        {
+            string pattern = @"([^a-zA-Z])";
+            string sentence = "";
+            string[] words = Regex.Split(line, pattern);
+            for (int i = 0; i < words.Length; ++i)
+            {
+                words[i] = ChangeColor(words[i], keywordsList);
+                sentence += words[i];
+            }
+            return sentence;
+        }
+
+        private string ChangeColor(string word, List<string> keywordsList)
+        {
+            if (keywordsList.Contains(word))
+                word = "<span>" + word + "</span>";
+            return word;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool stats = false,
+                 keywords = false;
+            List<string> keywordsList = new List<string>();
+            List<string> csFilesToConvert = new List<string>();
+
+            keywordsList = RemplirTableauKeyWord();
+            csFilesToConvert = AnalyseArguments(args, ref stats, ref keywords);
+            // ouvre les thread pour chaque element dans la List
+            List<CSFile> ListFiles = new List<CSFile>();  
+            for(int i = 0; i < csFilesToConvert.Count(); i++)
+            {
+                CSFile unFile = new CSFile(csFilesToConvert[i]);
+                ListFiles.Add(unFile);
+                ListFiles[i].CreateHTML(stats, keywords, keywordsList);
+            }
+            if (stats)
+                Statistiques(ListFiles[0].getStatsDictionary(), ListFiles[0].getNumberofNumber(), keywordsList);
+        }
+
+        // CRÉE LE FICHIER DE STATISTIQUES
+        private static void Statistiques(Dictionary<string, int> statistiques, int NumberOfNumber, List<string> keywordsList)
         {
             StreamWriter file = new StreamWriter("Statistiques.txt");
             var items = from pair in statistiques
@@ -45,42 +142,29 @@ namespace WebProjectPatrice
             file.Close();
         }
 
-        private static void FillStatsFile(string line)
+        // ANALYSE LES ARGUMENTS RECU DE L'UTILISATEUR
+        private static List<string> AnalyseArguments(string[] arguments, ref bool stats, ref bool keywords)
         {
-            string pattern = @"[^\w_]|(\d+\.\d+)";
-            string[] words = Regex.Split(line, pattern);
-            for (int i = 0; i < words.Length; ++i)
-            {
-                if(words[i] != "")
-                {
-                    if ((Regex.IsMatch(words[i], @"^\d+\.\d+")) || (Regex.IsMatch(words[i], @"^\d+")))
-                        NumberOfNumber++;
-                    if (!statistiques.ContainsKey(words[i]))
-                        statistiques.Add(words[i], 1);
-                    else
-                        statistiques[words[i]]++;
-                }
-            }
-        }
-
-        private static void AnalyseArguments(string[] arguments)
-        {
-            string extension = ""; 
+            List<string> csFilesToConvert = new List<string>();
+            string extension = "";
             foreach (string s in arguments)
             {
-                if (s == "-stats" || s == "/stats")
-                    stats = true; 
-                else if (s == "-keywords" || s == "/keywords")
+                if (s.ToLower() == "-stats" || s.ToLower() == "/stats")
+                    stats = true;
+                else if (s.ToLower() == "-keywords" || s.ToLower() == "/keywords")
                     keywords = true;
-                else if(s[0] != '-' && s[0] != '/')
+                else if (s[0] != '-' && s[0] != '/')
                     extension = Path.GetExtension(s);
-                    if (extension == ".cs")
-                        csFilesToConvert.Add(s);
+                if (extension == ".cs")
+                    csFilesToConvert.Add(s);
             }
+            return csFilesToConvert;
         }
 
-        private static void RemplirTableauKeyWord()
+        // REMPLI LA LIST DES KEYWORDS
+        private static List<string> RemplirTableauKeyWord()
         {
+            List<string> keywordsList = new List<string>();
             string line;
             StreamReader file = new StreamReader("KeyWords.txt");
             while ((line = file.ReadLine()) != null)
@@ -88,60 +172,7 @@ namespace WebProjectPatrice
                 if (line != "") keywordsList.Add(line);
             }
             file.Close();
-        }
-
-        private static void ReadEachLine()
-        {
-            string Input;
-            string TemplateStart = "<!DOCTYPE html>" + '\n' +
-                                      "<html>" + '\n' +
-                                      "<head>" + '\n' +
-                                      "<meta charset = 'utf-8' />" + '\n' +
-                                      "<style> span {color:blue;} </style>" +
-                                      "</head><body><pre>" + '\n' ; 
-            string TemplateEnd = "</pre></body>" + '\n' +
-                                 "</html >";
-            for (int i = 0; i < csFilesToConvert.Count ; ++i)
-            {
-                StreamWriter fileEnd = new StreamWriter(csFilesToConvert.ElementAt(i) + ".html");
-                fileEnd.WriteLine(TemplateStart);
-                StreamReader file = new StreamReader(csFilesToConvert.ElementAt(i));  
-                while ((Input = file.ReadLine()) != null)
-                {
-                    string v = Input;
-                    if (stats)
-                        FillStatsFile(v);
-                    v =  Input.Replace("&", "&amp;");
-                    v = v.Replace(">", "&gt;");
-                    v = v.Replace("<", "&lt;");
-                    if (keywords)
-                        v = AddColor(v);
-                    fileEnd.WriteLine(v);
-                }
-                fileEnd.WriteLine(TemplateEnd);
-                file.Close();
-                fileEnd.Close();
-            }
-        }
-
-        private static string AddColor(string line)
-        {
-            string pattern = @"([^a-zA-Z])";
-            string sentence = "";
-            string[] words = Regex.Split(line, pattern);
-            for(int i = 0 ; i < words.Length ; ++i)
-            {
-                words[i] = ChangeColor(words[i]);
-                sentence += words[i]; 
-            }
-            return sentence;
-        }
-
-        private static string ChangeColor(string word)
-        {
-            if (keywordsList.Contains(word, StringComparer.OrdinalIgnoreCase))
-                word = "<span>" + word + "</span>";      
-            return word; 
+            return keywordsList;
         }
     }
 }
